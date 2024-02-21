3b941c4cdfb96993e93219ccc7e6282e48fb8367
MarcoFonti <theemporiumg@gmail.com>
2024-02-21 15:17:56 +0100

    add scaffolding

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+$passwored = $_GET['passowrd'] ?? '';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="GET">
+        <input type="password" name="password">
+        <button>Invia</button>
+    </form>
+</body>
+</html>
